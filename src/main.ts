c17cc3f95c0cf4e069158a0d43efb24cae72d159
Matitojanu <mati.janus@gmail.com>
2024-07-18 22:35:33 +0200

    feat: add winston logging system

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './libs';
+import { WinstonModule } from 'nest-winston';
+import { instance } from 'logger/winston.logger';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      instance: instance,
+    }),
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   app.useGlobalFilters(new HttpExceptionFilter());
