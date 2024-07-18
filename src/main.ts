00be1710cfc1ed3e27cd896bc3bb968bf1a8b46e
Matitojanu <mati.janus@gmail.com>
2024-07-18 22:35:33 +0200

    parent a8bc1cabbd09957456eaca5859029c41487b4949 author Matitojanu <mati.janus@gmail.com> 1721334933 +0200 committer Matitojanu <mati.janus@gmail.com> 1727791652 +0200
    feat: add winston logging system
    
    fix: adjust logger in main.ts
    
    feat: add winston logging system
    
    feat: add saving logs to .log files
    
    fix: minor fixes
    
    fix: minor fixes

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import { ValidationPipe } from '@nestjs/common';
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './libs';
+import { WinstonModule } from 'nest-winston';
+import { WinstonLogger } from 'src/libs/internal/winston.logger';
+import { WinstonLogger } from 'src/libs/internal/winston.logger';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      instance: WinstonLogger,
+      instance: WinstonLogger,
+    }),
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(`${process.env.HOST_PORT}`);
 
-  const logger = new Logger('Bootstrap');
-  logger.log(
+  WinstonLogger.info(
     `Server running on: http://${process.env.HOST_NAME}:${process.env.HOST_PORT}`,
   );
 }
