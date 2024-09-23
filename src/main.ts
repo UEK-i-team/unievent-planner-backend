32ca96e7e7eb5063b8dc11979d1d43b0bafe095b
Matitojanu <mati.janus@gmail.com>
2024-09-23 18:45:04 +0200

    fix: minor fixes

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
-import { instance } from 'logger/winston.logger';
+import { WinstonLogger } from 'src/libs/internal/winston.logger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule, {
     logger: WinstonModule.createLogger({
-      instance: instance,
+      instance: WinstonLogger,
     }),
   });
+
+  try {
+    await app.listen('5304124');
+  } catch (Ex) {
+    throw Error('huh2');
+  }
+
   await app.listen(`${process.env.HOST_PORT}`);
 
-  instance.info(
+  WinstonLogger.info(
     `Server running on: http://${process.env.HOST_NAME}:${process.env.HOST_PORT}`,
   );
 }
