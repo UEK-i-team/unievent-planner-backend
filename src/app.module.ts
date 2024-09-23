33ea5ed2c43c932182dfb1cf7d744b9e7e64f351
seba1288 <seba128818@gmail.com>
2024-09-23 12:05:54 +0200

    feat: added filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { getMongoConnectionString } from './libs/internal';
+import { getMongoConnectionString } from './libs';
 
 @Module({
   imports: [
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './libs';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(`${process.env.HOST_PORT}`);
 
   const logger = new Logger('Bootstrap');
