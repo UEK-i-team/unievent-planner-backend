d1d7ae155a889404d2dc7dee24bbf15d7da3085e
seba1288 <seba128818@gmail.com>
2024-11-13 22:01:50 +0100

    fix: besic tests

--- src/libs/shared/enums/event-type-color.enum.ts
+++ src/libs/shared/enums/event-type-color.enum.ts
@@ -1,9 +1,7 @@
 export enum EventTypeColor {
   CLASSES = '#BBE2FF',
   CLASS_CANCELLATION = '#FFF069',
-  EXAM = '#F1656A',
+  TESTS = '#F1656A', //EXAM,RETAKES,QUIZ
   MEETING = '#B8FFA8',
   OTHER = '#D9D9D9',
-  QUIZ = '#F1656A',
-  RETAKE = '#F1656A',
 }
