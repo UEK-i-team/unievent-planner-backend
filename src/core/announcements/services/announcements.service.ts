05341f01ea4b1c22db5c584267c17037b610e47a
grochalapiotr <miami2042@gmail.com>
2024-12-18 16:19:56 +0100

    feat: changes in announcement service
    feat: add announcement endpoint

--- src/core/announcements/services/announcements.service.ts
+++ src/core/announcements/services/announcements.service.ts
@@ -8,7 +8,7 @@ import { plainToClass } from 'class-transformer';
 import { SystemLogsService } from 'src/core/system-logs/services/system-logs.service';
 import { UpserDefaultsService } from 'src/upser-defaults/upser-defaults.service';
 import { UserAccountDto } from 'src/core/accounts/dtos';
-import { SystemLogDto } from 'src/libs';
+import { Context, SystemLogAction, SystemLogDto } from 'src/libs';
 
 @Injectable()
 export class AnnouncementsService {
@@ -38,9 +38,9 @@ export class AnnouncementsService {
 
     const announcementLog: SystemLogDto = {
       // id: '',
-      action: 'create',
+      action: SystemLogAction.CREATE,
       message: `Announcement created: ${createAnnouncementDto.title}`,
-      context: 'announcement',
+      context: Context.ANNOUNCEMENT,
       createdBy: user,
       updatedBy: user,
       createdAt: new Date(),
@@ -48,6 +48,74 @@ export class AnnouncementsService {
     };
 
     await this.systemLogsService.createSystemLog(announcementLog);
+    return createAnnouncementDoc.save();
+  }
+
+  async find(): Promise<AnnouncementDto[]> {
+    const announcement = await this.announcementModel
+      .find()
+      .select('-__v')
+      .lean()
+      .exec();
+
+    return announcement.flat().map((el) => plainToClass(AnnouncementDto, el));
+  }
+
+  async findById(id: string): Promise<AnnouncementDto | null> {
+    const announcement = await this.announcementModel
+      .findById(id)
+      .select('-__v')
+      .lean()
+      .exec();
+
+    if (!announcement) {
+      throw new NotFoundException(`Annoncement with ID ${id} not found`);
+    }
+
+    return plainToClass(AnnouncementDto, announcement);
+  }
+
+  async deleteById(id: string): Promise<void> {
+    // TODO: check with token if user is the owner of announcement
+    const announcement = await this.announcementModel
+      .findOneAndDelete({ _id: id })
+      .exec();
+
+    if (!announcement) {
+      throw new NotFoundException(`Annoncement with ID ${id} not found`);
+    }
+  }
+}
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Announcement } from '../../../models/announcement.model';
+import { Model } from 'mongoose';
+import { CreateAnnouncementDto } from '../dtos';
+import { AnnouncementDto } from '../dtos';
+import { InjectModel } from '@nestjs/mongoose';
+import { plainToClass } from 'class-transformer';
+
+@Injectable()
+export class AnnouncementsService {
+  constructor(
+    @InjectModel(Announcement.name)
+    private announcementModel: Model<Announcement>,
+  ) {}
+
+  async create(
+    createAnnouncementDto: CreateAnnouncementDto,
+  ): Promise<Announcement> {
+    // TODO: add validation of user's username with token and assigne it below
+    const user: string = 'test';
+    const createAnnouncementDoc = new this.announcementModel();
+    createAnnouncementDoc.createdBy = user;
+    createAnnouncementDoc.updatedBy = user;
+    createAnnouncementDoc.title = createAnnouncementDto.title;
+    createAnnouncementDoc.description = createAnnouncementDto.description;
+    createAnnouncementDoc.expiresAt = createAnnouncementDto.expiresAt;
+    createAnnouncementDoc.important = createAnnouncementDto.important;
+    createAnnouncementDoc.createdAt = new Date();
+    createAnnouncementDoc.updatedAt = new Date();
+
     return createAnnouncementDoc.save();
   }
 
