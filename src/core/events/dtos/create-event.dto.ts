8f37a9af9fc0ff0fc1d86a67a7c90258bc2d5891
Oliwier Matus <matusoliwier@gmail.com>
2025-02-05 17:10:10 +0100

    fix: added constraints to dtos

--- src/core/events/dtos/create-event.dto.ts
+++ src/core/events/dtos/create-event.dto.ts
@@ -1,11 +1,15 @@
 import { Expose } from 'class-transformer';
 import { EventTypeDto } from './eventType.dto';
+import { FieldConstraints } from 'src/libs';
+import { MaxLength } from 'class-validator';
 
 export class CreateEventDto {
   @Expose()
+  @MaxLength(FieldConstraints.TITLE.MAX_LENGTH)
   title!: string;
 
   @Expose()
+  @MaxLength(FieldConstraints.DESCRIPTION.MAX_LENGTH)
   description!: string;
 
   @Expose()
